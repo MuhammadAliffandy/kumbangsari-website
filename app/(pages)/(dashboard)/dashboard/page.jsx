fff75c8d4b218f9308d9c0f9af0a8ccf1f059333
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2024-03-08 10:29:39 +0700

    updated  dashboard pages

---
+++ app/(pages)/(dashboard)/dashboard/page.jsx
@@ -0,0 +1,14 @@
+
+import { Box } from '@mui/material'
+
+const DashboardPage = () => {
+    return(
+        <>
+            <Box>
+                <h1 className='text-black' >DashBoard Page</h1>
+            </Box>
+        </>
+    ) 
+}
+
+export default  DashboardPage ;
