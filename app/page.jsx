3bfc36b80b0679392038b662a1116b4e1df70991
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2024-05-15 11:53:51 +0700

    feat: updated structure landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,116 +1,191 @@
-
+'use client'
 import Image from "next/image";
+import Box from '@mui/material/Box'
+import AppButton from "@/app/components/appButton/appButton";
+import AppCustomButton from "@/app/components/appButton/appCustomButton";
+import { motion } from 'framer-motion';
+import Link from "next/link";
+import { useState } from "react";
 
-export default function Home() {
+const items = [
+  {
+      name: 'Content Optimization',
+      description: 'Get content recommendations that best suit your product!',
+      image: '/images/vector/AI.png',
+  },
+  {
+      name: 'Time Efficiency',
+      description: 'Organize and upload all your product content from just one platform!',
+      image: '/images/vector/statistic.png',
+  },
+  {
+      name: 'Structured Scheduling',
+      description: 'Schedule the content you will upload with the auto-post feature!',
+      image: '/images/vector/content.png',
+  },
+];
+
+export default function LandingPage() {
+
+  // state animation
+  const [arrowIcon , setArrowIcon ] = useState('/images/icon/arrowUpRight.svg')
+  const [initButton , setInitButton] = useState(1)
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex flex-col w-full">
+      <nav className="flex items-center justify-around py-[15px] border-b-[1px] border-TEXT-4 border-opacity-25">
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <Box className='flex items-center gap-[10px]'>
+              <img className='w-[30px] h-[30px]' src='/images/icon/logo/planify.png' />
+              <p className='bg-gradient-to-b from-[#44B8F8] to-[#4E5FE5] text-transparent bg-clip-text ont-poppins text-[24px] font-extrabold'>Planify</p>
+          </Box>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <ul className=" text-[12px] flex gap-[30px] font-bold font-poppins">
+            <li className="text-TEXT-1 hover:text-PRIMARY-500"><a>Feature</a></li>
+            <li className="text-TEXT-1 hover:text-PRIMARY-500"><a>Subscribe</a></li>
+            <li className="text-TEXT-1 hover:text-PRIMARY-500"><a>FAQ</a></li>
+          </ul>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <Box className='flex items-center gap-[10px]'>
+              <AppButton
+                  className='px-[20px] py-[6px] text-[12px] bg-PRIMARY-100 text-PRIMARY-500 font-poppins rounded-[6px]'
+                  text={'Login'} 
+                  type = {'button'}
+                  onClick={()=>{
+                  }}
+              />
+              <AppCustomButton 
+                      onMouseEnter={()=>{setArrowIcon('/images/icon/arrowRight.svg')}} 
+                      onMouseLeave={()=>{setArrowIcon('/images/icon/arrowUpRight.svg')}} 
+                      className=' flex gap-[10px] items-center bg-PRIMARY-500 rounded-[6px] px-[20px] py-[6px] '
+                      onClick={()=>{
+                      }}
+                  >
+                    <p className="text-TEXT-5 text-[12px]">Register</p>
+                    <img src={arrowIcon} alt="alt-icon" />
+              </AppCustomButton>
+          </Box>
+      </nav>
+      {/*  */}
+      <section className="flex flex-col items-center justify-center h-[100vh]">
+        <Box className='flex flex-col gap-[15px] items-center justify-center w-[50%]'>
+            <p className="text-PRIMARY-500 text-[18px]">WELCOME TO PLANIFY</p>
+            <Box className='flex flex-col gap-[8px] text-[32px] items-center font-poppins font-extrabold'>
+              <p className="text-TEXT-1">One platform for all your</p>
+              <p className="text-PRIMARY-500">Social Media!</p>
+            </Box>
+            <p className="text-NEUTRAL-700 text-[18px] text-center"
+            >Planify is a content management platform that brings all your content together. With the help of Generate AI and Auto-Post, experience the ease of content management with us!</p>
+            <Box className='flex items-center gap-[10px]'>
+              <AppCustomButton className=' flex gap-[10px] items-center bg-PRIMARY-500 rounded-[6px] px-[24px] py-[10px] '
+                      onClick={()=>{
+                      }}
+                  >
+                    <p className="text-TEXT-5 text-[14px]">Get Started</p>
+                    <img src="/images/icon/sparkling-white.svg" />
+              </AppCustomButton>
+              <AppButton
+                  className='px-[24px] py-[10px] text-[14px] bg-PRIMARY-100 text-PRIMARY-500 font-poppins rounded-[6px]'
+                  text={'Our Feature'} 
+                  type = {'button'}
+                  onClick={()=>{
+                  }}
+              />
+          </Box>
+        </Box>
+      </section>
+      {/*  */}
+      <section className="flex flex-col items-center justify-center h-[100vh]">
+          <Box className='flex flex-col items-center gap-[100px]'>
+                <Box className='flex flex-col gap-[5px] items-center'>
+                  <p className="text-[18px] text-PRIMARY-500">Why Planify?</p>
+                  <p className="text-[24px] font-extrabold text-TEXT-1">Optimal, Efficient and Scheduled</p>
+                </Box>
+                <Box className='flex items-center gap-[40px] justify-center '>
+                  {
+                    items.map(data => {
+                      return(
+                        <Box className='flex flex-col gap-[10px] items-center w-[20%]'>
+                          <img className="w-[auto] h-[200px]" src={data.image} alt="picture-content" />
+                          <p className="text-[14px] text-TEXT-1 font-bold">{data.name}</p>
+                          <p className="text-[12px] text-TEXT-1 text-center">{data.description}</p>
+                        </Box>
+                      )
+                    })
+                  }
+                </Box>
+          </Box>
+      </section>
+      {/*  */}
+      <section className="flex flex-col items-center justify-center h-[100vh]">
+          <Box className='flex flex-col items-center gap-[100px]'>
+                <Box className='flex flex-col gap-[5px] items-center'>
+                  <p className="text-[18px] text-PRIMARY-500">How Do We Work?</p>
+                  <p className="text-[24px] font-extrabold text-TEXT-1">Our Features</p>
+                </Box>
+                
+                <Box className='flex items-center justify-center'>
+                  <motion.div 
+                      className='flex items-center bg-PRIMARY-100 text-[12px] gap-[18px] px-[10px] py-[10px] rounded-[8px]'
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{ duration: 0.5 }}
+                    >
+                      <motion.div 
+                        onClick={() => { setInitButton(1) }} 
+                        className={`flex items-center justify-start gap-[10px] cursor-pointer py-[10px] px-[20px] rounded-[6px] ${initButton != 1 ? '' : 'bg-PRIMARY-500'}`}
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0, opacity: 0.6 }}
+                      >
+                        <img className="w-[20px] h-[20px]" src={`/images/icon/landing-page/${initButton != 1 ? 'Sparkle.svg' : 'Sparkle-soft.svg'}`} alt="icon-button" />
+                        {initButton != 1 ? null : <p>Generate AI</p>}
+                      </motion.div>
+                      <motion.div 
+                        onClick={() => { setInitButton(2) }} 
+                        className={`flex items-center justify-start gap-[10px] cursor-pointer p-[10px] rounded-[6px] ${initButton != 2 ? '' : 'bg-PRIMARY-500'}`}
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0, opacity: 0.6 }}
+                      >
+                        <img className="w-[20px] h-[20px]" src={`/images/icon/landing-page/${initButton != 2 ? 'MagnifyingGlass.svg' : 'MagnifyingGlass-soft.svg'}`} alt="icon-button" />
+                        {initButton != 2 ? null : <p>Content Analysis</p>}
+                      </motion.div>
+                      <motion.div 
+                        onClick={() => { setInitButton(3) }} 
+                        className={`flex items-center justify-start gap-[10px] cursor-pointer p-[10px] rounded-[6px] ${initButton != 3 ? '' : 'bg-PRIMARY-500'}`}
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0, opacity: 0.6 }}
+                      >
+                        <img className="w-[20px] h-[20px]" src={`/images/icon/landing-page/${initButton != 3 ? 'ClockCountdown.svg' : 'ClockCountdown-soft.svg'}`} alt="icon-button" />
+                        {initButton != 3 ? null : <p>Auto-Post Content</p>}
+                      </motion.div>
+                  </motion.div>
+                  {/*  */}
+                  <Box className='flex items-center bg-PRIMARY-500 p-[14px]'>
+                      <Box className='flex items-center bg-PRIMARY-500'>
+                      </Box>        
+                        {/*  */}
+                        <Box>
+                            <Box>
+                              <img/>
+                              <p></p>
+                            </Box>
+                            <p></p>
+                            <AppCustomButton className=' flex gap-[10px] items-center bg-PRIMARY-500 rounded-[6px] px-[24px] py-[10px] '
+                                  onClick={()=>{
+                                  }}
+                              >
+                                <p className="text-TEXT-5 text-[14px]">Get Started</p>
+                                <img src="/images/icon/sparkling-white.svg" />
+                          </AppCustomButton>
+                        </Box>           
+                  </Box>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+                </Box>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          </Box>
+      </section>
+      {/*  */}
     </main>
   );
 }
--- middleware.js
+++ middleware.js
@@ -3,20 +3,20 @@ import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
 
-    const isLogin = req.cookies.get('token');
-    const { pathname } = req.nextUrl;
+    // const isLogin = req.cookies.get('token');
+    // const { pathname } = req.nextUrl;
 
-    const response = await fetch(`${process.env.BASE_URL_DEV}/api/v1/users`, {
-        headers: {
-            Authorization: `Bearer ${isLogin}`,
-        },
-    });
+    // const response = await fetch(`${process.env.BASE_URL_DEV}/api/v1/users`, {
+    //     headers: {
+    //         Authorization: `Bearer ${isLogin}`,
+    //     },
+    // });
         
-    if(!response.ok){
-        return NextResponse.redirect(new URL('/auth/signin', req.url));
-    }
+    // if(!response.ok){
+    //     return NextResponse.redirect(new URL('/auth/signin', req.url));
+    // }
     
-    return NextResponse.redirect(new URL('/dashboard', req.url));
+    // return NextResponse.redirect(new URL('/dashboard', req.url));
     
 }
 
