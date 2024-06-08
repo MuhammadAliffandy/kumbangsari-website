a471458e53038214fad74ff83cbc7835f83b1578
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2024-06-08 13:36:33 +0700

    feat : update middleware validation token

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { BASE_URL } from './app/utils/constants';
 
 export async function middleware(req) {
-    const isLogin = req.cookies.get('token') ?  req.cookies.get('token')?.value  : '';
+    const isLogin = req.cookies.get('token') ?  req.cookies.get('token')?.value  : null;
     const { pathname } = req.nextUrl;
     
     const response = await fetch(`${BASE_URL}/api/v1/users`, {
@@ -15,7 +15,6 @@ export async function middleware(req) {
         return NextResponse.redirect(new URL('/dashboard', req.url)); 
     }
     
-    console.log()
 
     if( pathname.includes('/dashboard') && response.status == 400 || isLogin == '' ){
         return NextResponse.redirect(new URL('/auth/signin', req.url)); 
